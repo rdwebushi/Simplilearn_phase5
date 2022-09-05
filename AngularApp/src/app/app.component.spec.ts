046f7b2034398078611cd4b316bb027d5fcb7541
rdwebushi <rushikesh26deshpande@gmail.com>
2022-09-05 12:47:21 +0530

    updated application

--- AngularApp/src/app/app.component.spec.ts
+++ AngularApp/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngulApp'`, () => {
+  it(`should have as title 'AngularApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngulApp');
+    expect(app.title).toEqual('AngularApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('AngulApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('AngularApp app is running!');
   });
 });
